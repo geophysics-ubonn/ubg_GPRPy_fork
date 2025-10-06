dceb9d63f3023d9e64c65931e81b741a40258cb4
Maximilian Weigand <mweigand@mweigand.net>
2025-10-06 08:50:03 +0200

    adapt metadata to make clear that this is a fork of GPRPy

--- setup.py
+++ setup.py
@@ -4,32 +4,37 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="gprpy",
-    version="1.0.14",
-    author="Alain Plattner",
-    author_email="plattner@alumni.ethz.ch",
+    name="ubg_gprpy_fork",
+    version="0.0.1",
+    author="Fork: Maximilian Weigand, Original GPRPy: Alain Plattner",
+    author_email="mweigand@geo.uni-bonn.de",
     description="".join((
         "GPRPy - open source ground penetrating radar processing ",
         "and visualization",
+        "Based on GPRPy (https://github.com/NSGeophysics/GPRPy)"
     )),
     entry_points={'console_scripts': ['gprpy = gprpy.__main__:main']},
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/NSGeophysics/GPRPy",
+    url="github.com/geophysics-ubonn/ubg_GPRPy_fork/",
     packages=['gprpy'],
-    package_data={'gprpy': ['exampledata/GSSI/*.DZT',
-                            'exampledata/GSSI/*.txt',
-                            'exampledata/SnS/ComOffs/*.xyz',
-                            'exampledata/SnS/ComOffs/*.DT1',
-                            'exampledata/SnS/ComOffs/*.HD',
-                            'exampledata/SnS/WARR/*.DT1',
-                            'exampledata/SnS/WARR/*.HD',
-                            'exampledata/pickedSurfaceData/*.txt',
-                            'examplescripts/*.py',
-                            'toolbox/splashdat/*.png',
-                            'toolbox/*.py',
-                            'irlib/*.py',
-                            'irlib/external/*.py']},
+    package_data={
+        'gprpy': [
+            'exampledata/GSSI/*.DZT',
+            'exampledata/GSSI/*.txt',
+            'exampledata/SnS/ComOffs/*.xyz',
+            'exampledata/SnS/ComOffs/*.DT1',
+            'exampledata/SnS/ComOffs/*.HD',
+            'exampledata/SnS/WARR/*.DT1',
+            'exampledata/SnS/WARR/*.HD',
+            'exampledata/pickedSurfaceData/*.txt',
+            'examplescripts/*.py',
+            'toolbox/splashdat/*.png',
+            'toolbox/*.py',
+            'irlib/*.py',
+            'irlib/external/*.py'
+        ]
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
